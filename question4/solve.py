6774d0b73cbf8faa6a28a6cb79b50cae6016ffe6
Harris <haf094@usask.ca>
2020-12-07 01:20:58 -0600

    missing some weird edge case idfk

--- question4/solve.py
+++ question4/solve.py
@@ -38,11 +38,67 @@ def find_users_with_required_fields(data, reqs):
             valid_users.append(user)
     return valid_users
 
+def find_users_with_valid_data(data):
+    """
+    this makes me wanna throw up
+     Ideally I would read these requirements from a config
+     but I wanna get this question done and I don't wanna fuck with getting that working
+     in case future me or someone else wants to do this for fun:
+        - store format as a csv with
+            field,min,max,format(regex pattern)
+        read these into a dict and then compare all the user data
+     """
+    valid_users = []
+    for user in data:
+        #validate the years first
+        if len(user["byr"]) > 4:
+            continue
+        if int(user["byr"]) < 1920 or int(user["byr"]) > 2002:
+            continue
+        if len(user["iyr"]) > 4:
+            continue
+        if int(user["iyr"]) < 2010 or int(user["iyr"]) > 2020:
+            continue
+        if len(user["eyr"]) > 4:
+            continue
+        if int(user["eyr"]) < 2020 or int(user["iyr"]) > 2030:
+            continue
+
+        # check that the height has a valid unit and value
+        height_unit = re.findall("[A-Za-z]+", user["hgt"])
+        height_val = re.findall("[\d]+", user["hgt"])
+        try:
+            if height_unit[0] == "in":
+                if int(height_val[0]) < 59 or int(height_val[0]) > 76:
+                    continue
+            elif height_unit[0] == "cm":
+                if int(height_val[0]) < 150 or int(height_val[0]) > 193:
+                    continue
+            else:
+                continue
+        except IndexError:
+            continue
+
+        # check hair color
+        if not bool(re.search("#([\d]|[a-f]){6}", user["hcl"])):
+            continue
+        # eye color
+        if not bool(re.search("(amb|blu|brn|gry|grn|hzl|oth)", user["ecl"])):
+            continue
+        # PID has 9 digits
+        if not bool(re.search("[\d]{9}", user["pid"])):
+            continue
+        valid_users.append(user)
+    return valid_users
+
+
+
 batch_path = sys.argv[1]
 reqs_path = sys.argv[2]
 data = get_data(batch_path)
-for user in data:
-    print(user)
 reqs = get_reqs(reqs_path)
-valid = find_users_with_required_fields(data, reqs)
+good_fields = find_users_with_required_fields(data, reqs)
+valid = find_users_with_valid_data(good_fields)
+for user in valid:
+    print(user)
 print("There are {0} valid users".format(len(valid)))
